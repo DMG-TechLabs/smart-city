c591afd5cf30445f8fa97e5b922253ce804635b5
Konstantinos Despoinidis <kdesp2003@gmail.com>
2025-04-03 14:38:53 +0300

    added admin user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
+import { ChartComponent } from '@/components/local/chart'
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div>
+        <h1>Hello World</h1>
+        <Button>Touch me</Button>
+        <ChartComponent />
+    </div>
   );
 }
