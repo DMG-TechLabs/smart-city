2e6813c5f017875f2d0bb1cc948f8827077c806d
Konstantinos Mokas <kostasmokas12@gmail.com>
2025-05-06 20:52:19 +0300

    Finish polling

--- database/main.go
+++ database/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -65,14 +66,34 @@ func RunAlerts(app *pocketbase.PocketBase, e *core.RecordEvent, alerts map[strin
 }
 
 func IfMetadata(app *pocketbase.PocketBase, e *core.RecordEvent) error {
-	app.Cron().MustAdd("", func() error {
+	app.Cron().MustAdd(e.Record.GetString("provider"), "*/2 * * * *", func() {
 		res, err := http.Get(e.Record.GetString("endpoint"))
 		if err != nil {
 			fmt.Println(err)
 		}
 
-		fmt.Println(res)
-		return nil
+		resBody := res.Body
+		var bodyBytes []byte
+		_, err = resBody.Read(bodyBytes)
+		var resJson map[string]interface{}
+		err = json.Unmarshal(bodyBytes, &resJson)
+
+		paths := e.Record.GetString("paths")
+		var pathsJson map[string]interface{}
+		err = json.Unmarshal([]byte(paths), &pathsJson)
+
+		provider := e.Record.GetString("provider")
+		collection, err := app.FindCollectionByNameOrId("api_" + provider)
+		record := core.NewRecord(collection)
+
+		for path, value := range pathsJson {
+			record.Set(path, resJson[value.(string)])
+		}
+
+		err = app.Save(record)
+		if err != nil {
+			fmt.Println(err)
+		}
 	})
 
 	return nil
