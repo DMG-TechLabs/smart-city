a38c68eb6d4f7903d8f66d38b5166caa1fa5b8da
Konstantinos Despoinidis <kdesp2003@gmail.com>
2025-06-01 21:31:17 +0300

    docs: added index.c

---
+++ docs/index.c
@@ -0,0 +1,37 @@
+#include <webc/webc-actions.h>
+#include <webc/webc-templates/pss.h>
+#include <webc/webc-server.h>
+
+int main(int argc, char** argv)
+{
+    WebcAction action = WEBC_ParseCliArgs(argc, argv);
+
+    char* desc = "A dynamic and customizable smart city dashboard";
+    ProjectShowcaseSite site = {
+        .project = (Project) {
+            .version = "1.0.0",
+            .lang = "NextJS",
+            .desc = desc,
+            .link = "https://github.com/DMG-TechLabs/smart-city",
+            .image = "./dashboard.png",
+            .license = "MIT",
+            .name = "Smart City",
+        },
+        .template = (Template) {
+            .year = 2025,
+            .author = "DMG-TechLabs",
+            .github_username = "DMG-TechLabs",
+            .email = "dmg.techlabs@gmail.com",
+            .about = desc,
+            .title = "Smart City",
+        },
+    };
+    char* buffer = WEBC_TemplateProjectShowcaseSite(site);
+
+    Tree tree = WEBC_MakeTree(".", 
+        WEBC_MakeRoute("/", buffer),
+        NULL
+    );
+
+    WEBC_HandleAction(action, tree);
+}
